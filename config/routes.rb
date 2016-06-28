62cbf6fc3920e233dbb552dfac73a9cf0a85eac4
sourav27 <debnath.sourabh@gmail.com>
2016-06-28 23:13:43 +0530

    added attachments feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :complaints
+  resources :complaints, only: [:index, :new, :create, :destroy, :show]
 
   root 'complaints#index'
 
