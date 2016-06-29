95ad3695bd9adaf56da23e0b71a164456d517b1f
sourav27 <debnath.sourabh@gmail.com>
2016-06-29 18:02:58 +0530

    started building app on rails 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :complaints
+	resources :complaints, only: [:index, :new, :create, :destroy, :show]
 
-  root 'complaints#index'
+	root 'complaints#index'
 
-  get 'login' => 'oauth#index'
-  delete 'logout'=>'oauth#signout'
+	get 'login' => 'oauth#index'
+	delete 'logout'=>'oauth#signout'
 end
