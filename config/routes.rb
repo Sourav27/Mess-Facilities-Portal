7859a8516839ccc875651c750258f878ef3065f1
sourav27 <debnath.sourabh@gmail.com>
2016-08-04 19:20:54 +0530

    added all complaints page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	resources :complaints, only: [:index, :new, :create, :destroy, :show]
-
+	get 'complaints_all' => 'complaints#all'
 	root 'complaints#index'
 
 	get 'login' => 'oauth#index'
